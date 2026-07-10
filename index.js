977a6cb8fd66d47670a99510137ed8ea505adead
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-07-10 16:51:51 +0600

    Logged out unauthorized access and checked isAdmin

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log('in verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized access!" });
+        return res.status(401).send({ message: "forbidden access!" });
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
@@ -63,10 +63,25 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin });
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
-      // give user entry if user doesn't exist
-      // It can be done in many ways: (1. unique field, 2. upsert 3. simple checking)
+      // give user entry if user doesn't exist:
+      // It can be done in many ways: (1. email unique, 2. upsert 3. simple checking)
       const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
