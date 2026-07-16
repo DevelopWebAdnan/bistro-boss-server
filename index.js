3925a8cb918e93f121423a1c0dacf752a2943f43
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-07-16 12:27:17 +0600

    Loaded data and displayed stats on the admin home

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ async function run() {
     })
 
     // stats or analytics
-    app.get('/admin-stats', async(req, res) => {
+    app.get('/admin-stats', async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
       const menuItems = await menuCollection.estimatedDocumentCount();
       const orders = await paymentCollection.estimatedDocumentCount();
@@ -252,7 +252,19 @@ async function run() {
       // const payments = await paymentCollection.find().toArray();
       // const revenue = payments.reduce( (total, payment) => total + payment.price, 0);
 
-      const result = paymentCollection.aggregate([])
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray();
+
+      const revenue = result.length > 0 ? result[0]?.totalRevenue : 0;
+      // const revenue = result[0]?.totalRevenue || 0;
 
       res.send({
         users,
